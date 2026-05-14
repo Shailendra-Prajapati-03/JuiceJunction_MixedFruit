a32182140f89e8408c816bf1d699c803125ec110
Shailendra-Prajapati-03 <shailendraprajapati640@gmail.com>
2026-05-14 16:59:06 +0530

    Add temporary debug endpoint to view live data

--- backend/juices/urls.py
+++ backend/juices/urls.py
@@ -5,7 +5,7 @@ from .views import (
     NotificationViewSet, GiftVoucherViewSet, RewardViewSet, CartViewSet,
     calculate_juice, apply_voucher, rewards_summary,
     vendor_register, VendorProductViewSet, VendorOrderViewSet, VendorViewSet,
-    send_otp, verify_otp, verify_registration, resend_otp,
+    send_otp, verify_otp, verify_registration, resend_otp, debug_data,
 )
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
@@ -39,4 +39,5 @@ urlpatterns = [
     path('auth/verify-otp/', verify_otp, name='verify_otp'),
     path('auth/resend-otp/', resend_otp, name='resend_otp'),
     path('verify-registration/', verify_registration, name='verify_registration'),
+    path('debug-data/', debug_data, name='debug_data'),
 ]
